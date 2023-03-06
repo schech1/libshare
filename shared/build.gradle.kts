b5c5c4c84af7d7a17a7a9ce738008aa57a0be9a3
schech1 <76683319+schech1@users.noreply.github.com>
2023-03-06 18:02:13 +0100

    Update build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -12,7 +12,7 @@ kotlin {
             }
         }
     }
-   // iosX64()
+    iosX64()
     iosArm64()
     iosSimulatorArm64()
 
@@ -35,21 +35,21 @@ kotlin {
         }
         val androidMain by getting
         val androidUnitTest by getting
-       // val iosX64Main by getting
+        val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
-         //   iosX64Main.dependsOn(this)
+            iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
         }
-     //   val iosX64Test by getting
+        val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
-       //     iosX64Test.dependsOn(this)
+            iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
